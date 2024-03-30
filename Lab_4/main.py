eb15044ec6bcb69445a7e8b0d472d098280c6080
4MC4 <marcin4c.mc@gmail.com>
2024-03-30 01:54:06 +0100

    Finished tasks 1-4 from lab 4

--- Lab_4/main.py
+++ Lab_4/main.py
@@ -10,6 +10,9 @@ from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 from sklearn.svm import SVC
 from sklearn.pipeline import make_pipeline
 from sklearn import impute
+from ydata_profiling import ProfileReport
+import seaborn as sns
+import scipy
 
 pd.set_option('display.max_columns', None)
 
@@ -49,7 +52,7 @@ def task1():
         if column != 'preg' and column != 'age':
             diabetes_no_missing.target = diabetes_no_missing.target[~(diabetes_no_missing.data[column] == 0)]
             diabetes_no_missing.data = diabetes_no_missing.data[~(diabetes_no_missing.data[column] == 0)]
-    print(diabetes_no_missing.data.describe())
+    #print(diabetes_no_missing.data.describe())
 
     # divide the data into training and testing - select all data from data apart from last column
     X_train_nm, X_test_nm, y_train_nm, y_test_nm = train_test_split(diabetes_no_missing.data, diabetes_no_missing.target, test_size=0.2, random_state=42)
@@ -79,8 +82,100 @@ def task1():
     imp_mean = impute.SimpleImputer(missing_values=0, strategy='mean')
     imp_mean.fit(diabetes.data.to_numpy()[:, 1:])
     diabetes_imp_mean = imp_mean.transform(diabetes.data.to_numpy()[:, 1:])
-    #print(pd.DataFrame(np.concatenate((diabetes.data.to_numpy()[:, 0], diabetes_imp_mean), axis= 0)).describe())
+    pregnancies = diabetes.data.to_numpy()[:, 0].reshape(-1, 1)
+    #print(pd.DataFrame(np.concatenate((pregnancies, diabetes_imp_mean), axis=1)).describe())
+    diabetes_imp_mean = pd.DataFrame(np.concatenate((pregnancies, diabetes_imp_mean), axis=1))
+    diabetes_imp_mean.columns = diabetes.data.columns
+    #print(diabetes_imp_mean.describe())
 
+    X_train_im, X_test_im, y_train_im, y_test_im = train_test_split(diabetes_imp_mean, diabetes.target, test_size=0.2, random_state=42)
+
+    # predict the target using the DecisionTreeClassifier
+    imputed_mean_DTC_clf = DecisionTreeClassifier()
+    imputed_mean_DTC_clf.fit(X_train_im.to_numpy(), y_train_im.to_numpy())
+    pred_imputed_mean_DTC = imputed_mean_DTC_clf.predict(X_test_im.to_numpy())
+    accuracy_imputed_mean_DTC = sklearn.metrics.accuracy_score(y_test_im, pred_imputed_mean_DTC)
+    print("Decision Tree Classifier with imputed mean values had accuracy of: "+str(accuracy_imputed_mean_DTC))
+
+    # predict the target using the RandomForestClassifier
+    imputed_mean_RFC_clf = RandomForestClassifier()
+    imputed_mean_RFC_clf.fit(X_train_im.to_numpy(), y_train_im.to_numpy())
+    pred_imputed_mean_RFC = imputed_mean_RFC_clf.predict(X_test_im.to_numpy())
+    accuracy_imputed_mean_RFC = sklearn.metrics.accuracy_score(y_test_im, pred_imputed_mean_RFC)
+    print("Random Forest Classifier with imputed mean values had accuracy of: "+str(accuracy_imputed_mean_RFC))
+
+    # predict the target using the SVC
+    imputed_mean_SVC_clf = SVC(gamma='auto')
+    imputed_mean_SVC_clf.fit(X_train_im.to_numpy(), y_train_im.to_numpy())
+    pred_imputed_mean_SVC = imputed_mean_SVC_clf.predict(X_test_im.to_numpy())
+    accuracy_imputed_mean_SVC = sklearn.metrics.accuracy_score(y_test_im, pred_imputed_mean_SVC)
+    print("Support Vector Classifier with imputed mean values had accuracy of: "+str(accuracy_imputed_mean_SVC))
+
+    # get profile report
+    profile = ProfileReport(diabetes.data, title="Profiling Report")
+    profile.to_file("your_report.html")
+
+def task2():
+    diabetes = fetch_openml("diabetes", version=1, as_frame=True)
+    #histogram = diabetes.data.mass.hist()
+    histogram = sns.histplot(data = diabetes.data, x = 'mass')
+    plt.show()
+    boxplot = sns.boxplot(data = diabetes.data, x = 'mass')
+    plt.show()
+
+def task3():
+    diabetes = fetch_openml("diabetes", version=1, as_frame=True)
+    plt.scatter(diabetes.data['plas'], diabetes.data['mass'])
+    plt.axvline(x=0)
+    plt.axhline(y=0)
+    plt.title('Pima Indians Diabetes Database')
+    plt.xlabel('plas')
+    plt.ylabel('mass')
+    plt.show()
+
+def task4():
+    diabetes = fetch_openml("diabetes", version=1, as_frame=True)
+    #check z-score of the data
+    # z_score = (diabetes.data - diabetes.data.mean())/diabetes.data.std()
+    z_score = scipy.stats.zscore(diabetes.data)
+    # save index of rows with z-score > 3
+    outliers = np.where(np.abs(z_score) > 3)
+    #print(outliers[0])
+    # remove rows with z-score > 3
+    diabetes.data = diabetes.data.drop(outliers[0])
+    diabetes.target = diabetes.target.drop(outliers[0])
+    #print(z_score)
+    #new_z_score = scipy.stats.zscore(diabetes.data)
+    #print(new_z_score)
+
+    # other possible ways:
+    # using sklearn.covariance.EllipticEnvelope
+
+    # # using sklearn.svm.OneClassSVM
+    # ocsvm = sklearn.svm.OneClassSVM(nu=0.2)
+    # ocsvm.fit(diabetes.data)
+    # # Predict the labels (1 for inliers, -1 for outliers)
+    # labels = ocsvm.predict(diabetes.data)
+    # outliers = np.where(np.array(labels) == -1)
+    # diabetes.data = diabetes.data.drop(outliers[0])
+    # diabetes.target = diabetes.target.drop(outliers[0])
+
+
+    # using sklearn.ensemble.IsolationForest
+    # using sklearn.neighbors.LocalOutlierFactor
+
+
+    # draw scatter plot again
+    plt.scatter(diabetes.data['plas'], diabetes.data['mass'])
+    plt.axvline(x=0)
+    plt.axhline(y=0)
+    plt.title('Pima Indians Diabetes Database')
+    plt.xlabel('plas')
+    plt.ylabel('mass')
+    plt.show()
 
 if __name__ == '__main__':
-    task1()
+    #task1()
+    #task2()
+    #task3()
+    task4()
